69d319f4b91f74765ab5d06598d2e387a7d3ea9a
jojooog <jooog0417@gmail.com>
2022-05-08 13:45:28 +0900

    @Primary RateDiscountPolicy refactoring

---
+++ src/main/java/hello/spring_core/discount/RateDiscountPolicy.java
@@ -0,0 +1,23 @@
+package hello.spring_core.discount;
+
+import hello.spring_core.member.Grade;
+import hello.spring_core.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
+public class RateDiscountPolicy implements DiscountPolicy{
+
+    private int discountRate = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if(member.getGrade() == Grade.VIP) {
+            return price * discountRate / 100;
+        } else {
+            return 0;
+        }
+    }
+}
