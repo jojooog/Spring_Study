580be9f529a17c9592d0944c495baa49a6e5117f
jojooog <jooog0417@gmail.com>
2022-05-08 13:28:19 +0900

    @Qualifier OrderServiceImpl refactoring

--- src/main/java/hello/spring_core/order/OrderServiceImpl.java
+++ src/main/java/hello/spring_core/order/OrderServiceImpl.java
@@ -6,17 +6,23 @@ import hello.spring_core.discount.RateDiscountPolicy;
 import hello.spring_core.member.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 
 @Component
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
 
      private final MemberRepository memberRepository;
      private final DiscountPolicy discountPolicy;
 
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, @Qualifier("main") DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
