9eae3a7d10033d23c7c71e66fea3aa27e10748c1
jojooog <jooog0417@gmail.com>
2022-05-11 22:17:03 +0900

    Spring lifecycle test

---
+++ src/test/java/hello/spring_core/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,35 @@
+package hello.spring_core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    public void LifeCycleTest(){
+        //ConfigurableApplicationContext는 ApplicationContext를 상속한다
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
+    }
+
+//1. 객체 생성 -> 의존관계 주입
+    @Configuration  //설정정보 등록
+    static class LifeCycleConfig{
+
+        @Bean
+        public NetworkClient networkClient(){
+            NetworkClient networkClient = new NetworkClient();
+            //객체 생성 당시에는 url이 없는 상태
+            //객체 생성과 초기화 하는 부분을 명확하게 나누는 것이 좋다
+            networkClient.setUrl("http://hello-spring");
+            return networkClient;
+        }
+
+
+    }
+}
