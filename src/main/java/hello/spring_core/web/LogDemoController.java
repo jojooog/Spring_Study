4b4e91f050fcc0d173635ded2e536cb7fa6d3432
jojooog <jooog0417@gmail.com>
2022-05-16 21:26:23 +0900

    LogDemoController ObjectProvider refactoring

--- src/main/java/hello/spring_core/web/LogDemoController.java
+++ src/main/java/hello/spring_core/web/LogDemoController.java
@@ -3,6 +3,7 @@ package hello.spring_core.web;
 
 import hello.spring_core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,13 +16,18 @@ import javax.servlet.http.HttpServletResponse;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request){
+
+        MyLogger myLogger = myLoggerProvider.getObject();
+
         String requestURL = request.getRequestURL().toString();
         myLogger.setRequestURL(requestURL);
+        //인터셉터나 서블릿 필터를 사용할 수도 있음
+
         myLogger.log("controller test");
         logDemoService.logic("testId");
         return "OK";
